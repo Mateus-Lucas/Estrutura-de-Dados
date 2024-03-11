671609c6ec0a4a92b657a17ef41a27c295a77e5a
Mateus-Lucas <mateus.lucas.ferreira.07@gmail.com>
2024-03-11 19:39:43 -0300

    Primeiros exemplos

---
+++ Condicionais/exemplo1.js
@@ -0,0 +1,8 @@
+
+var num = 0
+
+if (num == 1) {
+    console.log('numero é igual a 1');
+} else {
+    console.log('numero é diferente de 1');
+}
---
+++ Condicionais/exemplo2.js
@@ -0,0 +1,9 @@
+//Operador ternário
+
+var num = 1
+
+if (num === 1) {
+    num--
+} else {
+    num++
+}
